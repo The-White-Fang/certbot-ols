3c0a456606c0f243fb3d68645da492a66356ba1e
= <admin@thewhitefang.xyz>
2022-02-12 11:00:50 +0530

    added cert request logging

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const PORT = process.env.CERTBOT_PORT || 4321;
 const app = express();
 
 app.use((req, res, next) => {
-	console.log(req.headers);
+	console.log(`Certificate verification request received for host: ${req.headers.host}`);
 	next();
 });
 
-app.use('/', express.static('./challenges'));
+app.use('/.well-known', express.static('./challenges'));
 
 app.listen(PORT, () => {
 	console.log('CERTBOT started listening');
